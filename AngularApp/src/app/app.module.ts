23adcc781b43c82d508fbaccd2fb031f057ffcd2
Bhushan-K <bkhaladkar@yahoo.com>
2020-01-19 00:38:54 +0530

    commit

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserSignInComponent } from './user-sign-in/user-sign-in.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes : Routes=[
+  {path:'user-reg', component:UserRegistrationComponent},
+  {path:'user-signIn', component:UserSignInComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    UserSignInComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,{enableTracing:true}
+    ),
     BrowserModule,
     AppRoutingModule
   ],
